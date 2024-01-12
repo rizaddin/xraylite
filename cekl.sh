b49dfc71f6c0978f03eddafbbd7f93825f0caeac
rizaddin <150619322+rizaddin@users.noreply.github.com>
2024-01-13 00:36:50 +0700

    Update cekl.sh

--- cekl.sh
+++ cekl.sh
@@ -64,7 +64,7 @@ done
 rm -rf /tmp/other.txt
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}               •  JOYS VPN  •                  $COLOR1│$NC"
+echo -e "$COLOR1│${NC}                 •  JOYS VPN  •                  $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
