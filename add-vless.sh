2aebfeb7a6c867ec38f864c368e2e14ebc2208d9
rizaddin <150619322+rizaddin@users.noreply.github.com>
2024-01-12 23:20:39 +0700

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+NC="\e[0m"
+COLOR1="\033[0;31m" 
+###########- END COLOR CODE -##########
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 
 # Getting
