686df87bea820b3e20e4cbc6a123bb46713b1792
rizaddin <150619322+rizaddin@users.noreply.github.com>
2024-01-13 00:08:08 +0700

    Update menu-vless.sh

--- menu-vless.sh
+++ menu-vless.sh
@@ -32,7 +32,7 @@ echo -e " $COLOR1│$NC                                              ${NC} $COLO
 echo -e " $COLOR1│$NC   ${COLOR1}[00]${NC} • GO BACK${NC}                              $COLOR1│$NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}               •  Joys VPN  •                  $COLOR1│$NC"
+echo -e "$COLOR1│${NC}                 •  Joys VPN  •                  $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e ""
 read -p " Select menu :  "  opt
